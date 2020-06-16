e5d50796231d71df8a06c2c93470507c32c0fa09
Razielini <razielco@gmail.com>
2020-06-15 19:45:08 -0600

    Update question.js

--- controllers/question.js
+++ controllers/question.js
@@ -1,22 +1,38 @@
 'use strict'
 
+const { writeFile } = require('fs')
+const { promisify } = require('util')
+const { join } = require('path')
+
 const questions = require('../models/index').questions
+//const uuid = require('uuid/dist/v1')
+const { v1: uuidv1 } = require('uuid')
+
+const write = promisify(writeFile)
 
 const createQuestion = async (req, h) => {
   if(!req.state.user) return h.redirect('/login')
-  let result
+  let result, filename
   try {
-    result = await questions.create(req.payload, req.state.user)
-    console.log(`Pregunta creado con el ID: ${result}`)
+    if (Buffer.isBuffer(req.payload.image)) {
+      filename = `${uuidv1()}.png`
+      await write(join(__dirname, '..', 'public', 'uploads', filename), req.payload.image)
+    }
+
+    result = await questions.create(req.payload, req.state.user, filename)
+    req.log('info', `Pregunta creado con el ID: ${result}`)
+    //console.log(`Pregunta creado con el ID: ${result}`)
   } catch (error) {
-    console.log('Error :: createQuestion ::', error.message)
-    return history.view('ask', {
+    //console.log('Error :: createQuestion ::', error.message)
+    req.log('Error', error.message)
+    return h.view('ask', {
       title: 'Pregunta',
       error: 'Problemas creando la pregunta'
     }).code(500).takeover()
   }
 
-  return h.response(`Pregunta creado con el ID: ${result}`)
+  //return h.response(`Pregunta creado con el ID: ${result}`)
+  return h.redirect(`/question/${result}`)
 }
 
 const answerQuestion = async (req, h) => {
